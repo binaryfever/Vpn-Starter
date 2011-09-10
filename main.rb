135f37d61e29e03f51dd10475fd3fe5511654415
Fred McHale <binaryfever@gmail.com>
2011-09-10 09:12:00 -0700

    Remove unneeded comments

--- main.rb
+++ main.rb
@@ -1,19 +1,16 @@
 require 'tray_application.rb'; 
 require 'soft_token.rb';
 
-#create the application object
 app = TrayApplication.new("SoftToken II")
 
-#Set the icon filename
 app.icon_filename = 'lock.png'
 
-#Start the app
 app.item('Start'){
   SoftToken.start
 }
 
-#Exit the app
 app.item('Exit'){
   java.lang.System::exit(0)
 }
+
 app.run
